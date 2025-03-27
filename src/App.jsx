890de7a9f49178e5d322252087816048ae7d22ed
vikileon <daria.tim1@yahoo.com>
2025-03-27 21:40:35 +0500

    optimized code using custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,26 @@
-import { useRef, useState, useCallback, useEffect } from 'react';
+import { useRef, useState, useCallback } from 'react';
 
-import Places from './components/Places.jsx';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
-import { fetchUserPlaces, updateUserPlaces } from './http.js';
+import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import ErrorPage from './components/ErrorPage.jsx';
+import Modal from './components/Modal.jsx';
+import Places from './components/Places.jsx';
+import { useFetch } from './hooks/useFetch.js';
+import { fetchUserPlaces, updateUserPlaces } from './http.js';
 
 function App() {
   const selectedPlace = useRef();
-
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false); //the loading state, which you use to tell the user that you are currently fetching data
-  const [error, setError] = useState();
-
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const userPlaces = await fetchUserPlaces();
-        setUserPlaces(userPlaces);
-      } catch (error) {
-        setError({ message: error.message || 'Failed to fetch user places.' });
-      }
-
-      setIsFetching(false);
-    }
-
-    fetchPlaces();
-  }, []);
+  const {
+    isFetching,
+    error,
+    fetchedData: userPlaces,
+    setFetchedData: setUserPlaces,
+  } = useFetch(fetchUserPlaces, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -85,7 +72,7 @@ function App() {
       }
       setModalIsOpen(false);
     },
-    [userPlaces]
+    [userPlaces, setUserPlaces]
   );
 
   function handleError() {
@@ -120,15 +107,19 @@ function App() {
         </p>
       </header>
       <main>
-        {error && <ErrorPage title='An error occurred!' message={error.message} />}
-        {!error && <Places
-          title="I'd like to visit ..."
-          fallbackText='Select the places you would like to visit below.'
-          isLoading={isFetching}
-          loadingText="Fetching your places..."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />}
+        {error && (
+          <ErrorPage title='An error occurred!' message={error.message} />
+        )}
+        {!error && (
+          <Places
+            title="I'd like to visit ..."
+            fallbackText='Select the places you would like to visit below.'
+            isLoading={isFetching}
+            loadingText='Fetching your places...'
+            places={userPlaces}
+            onSelectPlace={handleStartRemovePlace}
+          />
+        )}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,42 +1,31 @@
-import { useEffect, useState } from 'react';
-
 import { fetchAvailablePlaces } from '../http.js';
 import { sortPlacesByDistance } from '../loc.js';
+import { useFetch } from '../hooks/useFetch.js';
 import ErrorPage from './ErrorPage.jsx';
 import Places from './Places.jsx';
 
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise(resolve => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
+
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false); //the loading state, which you use to tell the user that you are currently fetching data
-  const [availablePlaces, setAvailablePlaces] = useState([]); //data state, where you store the data that you fetching
-  const [error, setError] = useState(); //the error state which you use to show potentional errors on the screen
-
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-
-      try {
-        const places = await fetchAvailablePlaces();
-
-        navigator.geolocation.getCurrentPosition(position => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError({
-          message:
-            error.message || 'Could not fetch places, please try again later.',
-        });
-        setIsFetching(false);
-      }
-    }
-
-    fetchPlaces();
-  }, []);
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <ErrorPage title='An error occurred!' message={error.message} />;
